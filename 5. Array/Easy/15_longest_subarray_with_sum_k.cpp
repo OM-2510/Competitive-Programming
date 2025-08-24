3c15ebd98226366e3a6fb49a10b5f26438d1468f
Om <143173536+OM-2510@users.noreply.github.com>
2025-08-24 08:41:57 +0000

    24-09-2025

--- 5. Array/Easy/15_longest_subarray_with_sum_k.cpp
+++ 5. Array/Easy/15_longest_subarray_with_sum_k.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <map>
+#include <algorithm>
+
+int longestSubarray(std::vector<int> vec, int target){
+    std::unordered_map<int, int> array;
+    for(int i=0; i<vec.size(); i++){
+        if(array.back() == target){
+            return 
+        }
+    }
+
+
+
+}
+
+
+int main(){
+    std::vector <int> arr = {10,5,2,7,1,9};
+    int target = 15;
+    std::cout<<longestSubarray(arr, target);
+
+}
